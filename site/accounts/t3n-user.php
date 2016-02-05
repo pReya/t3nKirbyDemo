e4f6a01468cc6528b1e0ea56f505f7fc90170252
Moritz Stückler <moritz.stueckler@gmail.com>
2016-02-05 18:27:25 +0100

    Panel installiert, htaccess angepasst

---
+++ site/accounts/t3n-user.php
@@ -0,0 +1,8 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: t3n-user
+email: moritz.stueckler@gmail.com
+password: >
+  $2a$10$zaGuqKNSJK82G7HD677.6.fck6qsRgARHGkXW54Qv80PPiHRjTg72
+language: de
+role: admin
